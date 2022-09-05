10fde4d0ab3347e3d472af36b7bb9d4477ec8b41
Beastly4 <49074447+Beastly4@users.noreply.github.com>
2022-09-05 00:11:21 +0000

    Deploying to gh-pages from @ Beastly4/test@d17826bad4f67ad1ecaac8c541c544500edafe2e 🚀

--- _next/static/Huf__1FeVCY-z4oKWICVg/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/681-05d376264634f5b0.js","static/css/7c627bd4a570ffe4.css","static/chunks/pages/index-54b8f32657ab0d79.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
